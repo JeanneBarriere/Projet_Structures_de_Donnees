a6ec92d8f3fdf6402a7db7d22a093ae7236fc2fb
JeanneBarriere <barriere.jeanne@gmail.com>
2020-03-18 13:07:16 +0100

    test

--- Liste chainée/biblio_liste.c
+++ Liste chainée/biblio_liste.c
@@ -149,8 +149,8 @@ void recherche_doublons(Biblio * B){
 		int vis =0;
 		CellMorceau * l3=B->L;
 		while(l3){
+			printf("test\n" );
 			if (((strcmp(l2->artiste, l3->artiste))==0)&&((strcmp(l2->titre, l3->titre))==0)&&l2->num != l3->num){
-				printf("test\n" );
 				vis=1;
 			}
 			l3=l3->suiv;
